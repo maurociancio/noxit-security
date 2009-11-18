f3550bf3f804ed3bcf55fe7f88de1383e4ee5c78
Mauro Ciancio <maurociancio@gmail.com>
2009-11-18 19:51:55 -0300

    security project: started

---
+++ src/src/main/java/ar/noxit/security/App.java
@@ -0,0 +1,12 @@
+package ar.noxit.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
