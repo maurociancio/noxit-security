c3e65e791466c04a15b15530c1e466390e4e4297
Mauro Ciancio <maurociancio@gmail.com>
2009-11-22 14:44:57 -0300

    array based authenticator test

---
+++ src/src/test/java/ar/noxit/security/tests/ArrayBasedAuthorizerTest.java
@@ -0,0 +1,44 @@
+package ar.noxit.security.tests;
+
+import ar.noxit.security.auth.Authorizer;
+import ar.noxit.security.auth.authorization.ArrayBasedAuthorizer;
+import ar.noxit.security.exceptions.AuthException;
+import ar.noxit.security.exceptions.NotAuthenticatedException;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ArrayBasedAuthorizerTest {
+
+    private Authorizer authorizer;
+
+    @Test
+    public void testOkArrayBasedAuthorizer() throws AuthException {
+        this.authorizer = new ArrayBasedAuthorizer("create");
+
+        this.authorizer.authorize(new String[]{"create"});
+        this.authorizer.authorize(new String[]{"create"});
+    }
+
+    @Test
+    public void testOkUsingArrayForArrayBasedAuthorizer() throws AuthException {
+        this.authorizer = new ArrayBasedAuthorizer(new String[]{"read", "modify"});
+
+        this.authorizer.authorize(new String[]{"read"});
+        this.authorizer.authorize(new String[]{"modify"});
+    }
+
+    @Test(expected = NotAuthenticatedException.class)
+    public void testFailUsingArrayForArrayBasedAuthorizer() throws AuthException {
+        this.authorizer = new ArrayBasedAuthorizer(new String[]{"read", "modify"});
+
+        this.authorizer.authorize(new String[]{"erase"});
+    }
+
+    @Test(expected = NotAuthenticatedException.class)
+    public void testFailArrayBasedAuthorizer() throws AuthException {
+        this.authorizer = new ArrayBasedAuthorizer("list");
+
+        this.authorizer.authorize(new String[]{"create"});
+    }
+}
