862f6129f58e22c9a2a2c7375bff4b1924802e4c
Mauro Ciancio <maurociancio@gmail.com>
2009-11-22 12:50:38 -0300

    basic security commmand implementation

---
+++ src/src/main/java/ar/noxit/security/interceptors/commands/SecurityImplementorCommand.java
@@ -0,0 +1,64 @@
+package ar.noxit.security.interceptors.commands;
+
+import ar.noxit.security.annotations.Authenticate;
+import ar.noxit.security.annotations.Rol;
+import ar.noxit.security.auth.Authorizer;
+import ar.noxit.security.exceptions.AuthException;
+import ar.noxit.security.exceptions.AuthRuntimeException;
+import ar.noxit.security.exceptions.NotAuthenticatedException;
+import java.lang.reflect.Method;
+
+public class SecurityImplementorCommand<T> extends TemplateSecurityImplementorCommand<T> {
+
+    public SecurityImplementorCommand(Class<T> interfaze, T proxied) {
+        super(interfaze, proxied);
+    }
+
+    @Override
+    protected void authenticate() {
+    }
+
+    @Override
+    protected void authorizate(Class<T> interfaze, Method method) throws AuthException {
+        Authenticate classAuthenticateAnnotation = interfaze.getAnnotation(Authenticate.class);
+        Authenticate methodAuthenticateAnnotation = method.getAnnotation(Authenticate.class);
+
+        if (methodAuthenticateAnnotation == null && classAuthenticateAnnotation == null) {
+            throw new NotAuthenticatedException();
+        }
+
+        Authenticate authenticateAnnotation = classAuthenticateAnnotation != null
+                ? classAuthenticateAnnotation
+                : methodAuthenticateAnnotation;
+
+
+        Authorizer authorizer = instantiateAuthorizer(authenticateAnnotation);
+
+        Rol rolAnnotation = method.getAnnotation(Rol.class);
+
+        authorizer.authorize(getRolesFrom(rolAnnotation));
+    }
+
+    @Override
+    protected void checkPermissions() {
+    }
+
+    private Authorizer instantiateAuthorizer(Authenticate authenticateAnnotation) {
+        Class<? extends Authorizer> authorizer = authenticateAnnotation.authorizer();
+
+        try {
+            return authorizer.newInstance();
+        } catch (Exception ex) {
+            throw new AuthRuntimeException("Cannot instantiate " +
+                    authorizer.getName() + " using the default constructor", ex);
+        }
+    }
+
+    private String[] getRolesFrom(Rol rol) {
+        if (rol == null) {
+            return new String[0];
+        } else {
+            return rol.roles();
+        }
+    }
+}
