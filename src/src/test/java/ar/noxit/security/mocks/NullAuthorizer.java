90b6154a8655c7cbe7b1c222cd0ae7330d390826
Mauro Ciancio <maurociancio@gmail.com>
2009-11-22 22:53:16 -0300

    fix method throws

--- src/src/test/java/ar/noxit/security/mocks/NullAuthorizer.java
+++ src/src/test/java/ar/noxit/security/mocks/NullAuthorizer.java
@@ -1,11 +1,10 @@
 package ar.noxit.security.mocks;
 
 import ar.noxit.security.auth.Authorizer;
-import ar.noxit.security.exceptions.NotAuthenticatedException;
 
 public class NullAuthorizer implements Authorizer {
 
     @Override
-    public void authorize(String[] serviceRoles) throws NotAuthenticatedException {
+    public void authorize(String[] serviceRoles) {
     }
 }
