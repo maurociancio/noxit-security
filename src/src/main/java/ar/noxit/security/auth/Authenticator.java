5d0c062b18ff8bf1ec7691fe9444c3b9c1f09b0b
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 15:14:26 -0300

    authenticate throws not authenticated exception

--- src/src/main/java/ar/noxit/security/auth/Authenticator.java
+++ src/src/main/java/ar/noxit/security/auth/Authenticator.java
@@ -1,6 +1,8 @@
 package ar.noxit.security.auth;
 
+import ar.noxit.security.exceptions.NotAuthenticatedException;
+
 public interface Authenticator {
 
-    void authenticate();
+    void authenticate() throws NotAuthenticatedException;
 }
