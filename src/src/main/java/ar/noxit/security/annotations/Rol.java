049a7af2b209dc82460afc7432df43cd7744ce49
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 15:45:56 -0300

    rol javadoc

--- src/src/main/java/ar/noxit/security/annotations/Rol.java
+++ src/src/main/java/ar/noxit/security/annotations/Rol.java
@@ -5,9 +5,19 @@ import static java.lang.annotation.RetentionPolicy.*;
 import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Target;
 
+/**
+ * Use this annotation to mark a method or a class that needs roles in order
+ * to be executed.
+ *
+ * @author Mauro Ciancio
+ */
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
 public @interface Rol {
 
+    /**
+     * Roles required to execute the service.
+     * @return String[]
+     */
     String[] roles() default {};
 }
