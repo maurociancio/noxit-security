96f8570fc16bc809657658daf441ca50c6d851b8
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 15:55:10 -0300

    auth javadoc

--- src/src/main/java/ar/noxit/security/annotations/Auth.java
+++ src/src/main/java/ar/noxit/security/annotations/Auth.java
@@ -5,11 +5,29 @@ import static java.lang.annotation.RetentionPolicy.*;
 import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Target;
 
+/**
+ * Using this annotation can mark an interface (or a method) that requires both authorization
+ * and authentication.
+ *
+ * @author Mauro Ciancio
+ */
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
 public @interface Auth {
 
+    /**
+     * Authorize annotation. This annotation specifies what class should implement the
+     * authorization.
+     *
+     * @return Authorize
+     */
     Authorize authorize();
 
+    /**
+     * Authenticate annotation. This annotation specifies what class should implement the
+     * authentication.
+     *
+     * @return Authorize
+     */
     Authenticate authenticate();
 }
