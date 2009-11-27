d3cd96bb5bc44cf7eda1cd0432b0252823542f24
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 01:48:12 -0300

    implement getauthenticate in annotations finder to look for authenticate annotation

--- src/src/main/java/ar/noxit/security/interceptors/commands/AnnotationsFinder.java
+++ src/src/main/java/ar/noxit/security/interceptors/commands/AnnotationsFinder.java
@@ -1,10 +1,11 @@
 package ar.noxit.security.interceptors.commands;
 
+import ar.noxit.security.annotations.Authenticate;
 import ar.noxit.security.annotations.Authorize;
 import ar.noxit.security.annotations.Rol;
-import ar.noxit.security.auth.Authenticator;
 import ar.noxit.security.exceptions.AuthException;
 import ar.noxit.security.exceptions.NoRolException;
+import ar.noxit.security.exceptions.NotAuthenticatedException;
 import ar.noxit.security.exceptions.NotAuthorizatedException;
 import java.lang.reflect.Method;
 
@@ -19,8 +20,19 @@ public class AnnotationsFinder {
         this.interfaze = interfaze;
     }
 
-    public Authenticator getAuthenticator(Method method) {
-        return null;
+    public Authenticate getAuthenticator(Method method) throws AuthException {
+        // look for class based or method based authenticate annotation
+        Authenticate classAuthAnnotation = interfaze.getAnnotation(Authenticate.class);
+        Authenticate methodAuthAnnotation = method.getAnnotation(Authenticate.class);
+
+        // raise an exception if both annotations are null
+        if (methodAuthAnnotation == null && classAuthAnnotation == null) {
+            throw new NotAuthenticatedException("Neither method=[" + method.getName() + "] nor class=[" +
+                    interfaze.getName() + "] has authenticate annotation.");
+        }
+
+        // choose method annotation if both are present
+        return getWithMethodPriority(methodAuthAnnotation, classAuthAnnotation);
     }
 
     public Authorize getAuthorizer(Method method) throws AuthException {
