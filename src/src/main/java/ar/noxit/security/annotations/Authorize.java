0551bbaa6d9aff31b45dd4979c323f9ab073e5ce
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 15:41:06 -0300

    authorize javadoc

--- src/src/main/java/ar/noxit/security/annotations/Authorize.java
+++ src/src/main/java/ar/noxit/security/annotations/Authorize.java
@@ -7,9 +7,22 @@ import static java.lang.annotation.RetentionPolicy.*;
 import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Target;
 
+/**
+ * Use this annotation to mark an interface that requires authorization.
+ *
+ * If you need both authentication and authorization in your interface see Auth annotation.
+ *
+ * @see Auth
+ * @author Mauro Ciancio
+ */
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
 public @interface Authorize {
 
+    /**
+     * Specify which class should do the authorization
+     *
+     * @return Authorizer
+     */
     Class<? extends Authorizer> authorizer() default NullAuthorizer.class;
 }
