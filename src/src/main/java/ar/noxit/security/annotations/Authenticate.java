3b3e681b6534fadc9c9149754fb115e1f93cb1d3
Mauro Ciancio <maurociancio@gmail.com>
2009-11-27 15:37:28 -0300

    authenticate javadoc

--- src/src/main/java/ar/noxit/security/annotations/Authenticate.java
+++ src/src/main/java/ar/noxit/security/annotations/Authenticate.java
@@ -7,9 +7,22 @@ import static java.lang.annotation.RetentionPolicy.*;
 import static java.lang.annotation.ElementType.*;
 import java.lang.annotation.Target;
 
+/**
+ * Use this annotation to mark an interface that requires authentication.
+ *
+ * If you need both authentication and authorization in your interface see Auth annotation.
+ *
+ * @see Auth
+ * @author Mauro Ciancio
+ */
 @Target({TYPE, METHOD})
 @Retention(RUNTIME)
 public @interface Authenticate {
 
+    /**
+     * Specify which class should do the authentication
+     *
+     * @return Authenticator
+     */
     Class<? extends Authenticator> authenticator() default NullAuthenticator.class;
 }
